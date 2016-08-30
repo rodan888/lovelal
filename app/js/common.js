383bc53efe7d1f1682f2757166fc5422e5f0bcfc
rodan888 <tsumbaluk888@gmail.com>
2016-08-30 17:44:46 +0300

    input range add

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,143 @@
 $(function() {
+	var main = {
+		opt: {
+			wind: $(window),
+			img: $('img'),
+			linc: $('a'),
+			butSearch: $('#search'),
+			autoriz: $('.autoriz'),
+			filterB: $('.filters'),
+			magnific: $('.popup-slider'),
+			magnificConf: {
+				delegate: 'a',
+				type: 'image',
+				gallery: {
+					enabled:true
+				}
+			},	
+			range: $('.double-handle-slider'),
+			rangeMin: $('.min-value'),
+			rangeMax: $('.max-value'),
+			rangeConfig: {
+				start: [ 0, 100 ],
+				connect: true,
+				tooltips: false,
+				step: 1,
+				range: {
+					'min': [ 0 ],
+					'max': [ 100 ]
+				},
+				format: {
+					to: function(value) {
+						return value;
+					},
+					from: function(value) {
+						return value;
+					}
+				}
+			}	
+		},
+		// rangeInput: function(){	
+		// 	noUiSlider.create( main.opt.range, main.opt.rangeConfig);	
+			
+		// 	main.opt.range.noUiSlider.on('change', function( values, handle ) { 			
+		// 		var rangeValues = values;
+		// 		main.opt.rangeMin.value = rangeValues[0];
+		// 		main.opt.rangeMax.value = rangeValues[1];
+			
+		// 		var val = values[handle];			
+		// 		if(handle) {
+		// 			main.opt.rangeMax.value = Math.round(val);
+		// 		} else {
+		// 			main.opt.rangeMin.value = Math.round(val);
+		// 		}
+		// 	});	
+		// 	main.opt.rangeMin.addEventListener('change', function(){
+		// 		main.opt.range.noUiSlider.set([this.value, null]);
+		// 	});				
+		// 	main.opt.rangeMax.addEventListener('change', function(){
+		// 		main.opt.range.noUiSlider.set([null, this.value]);
+		// 	});
+		// },
+		filter: function(){
+			this.opt.butSearch.on('click',function(){
+				$(this).addClass('active');
+				main.toggleC(main.opt.filterB);			
+
+				main.opt.filterB.find('.container')
+					.append('<span class="close"><i class="fa fa-times"></i></span>');
+
+				$('.close').on('click',function(){
+					main.opt.butSearch.removeClass('active');
+					main.toggleC(main.opt.filterB);			
+					$(this).detach();
+				});
+			});
+		},
+		logIn: function(){
+			var loginB = this.opt.autoriz.find('a');
+			loginB.on('click', function(event){
+				event.preventDefault();
+				var t     = $(this),
+					  dataForm = t.data('form');
+					
+				loginB.not(t).removeClass('active');
+				t.toggleClass('active');
+				main.opt.autoriz.find('form').not('#'+dataForm).removeClass('active');			
+				$('#'+dataForm).toggleClass('active');
+			});
+		},		
+		toggleC: function(el){
+			el.toggleClass('active');
+		},
+		winH: function(){			
+			return this.opt.wind.height();
+		},
+		fullHeight: function(el){
+			$(el).css('min-height',main.winH()+'px');
+		},
+		dragstart: function(el){
+			$(el).on('dragstart',function(event){
+				event.preventDefault();
+			});
+		},
+		init: function(){
+			// default functions
+			this.dragstart(this.opt.img);
+			this.dragstart(this.opt.linc);
+
+			//Magnific popup slider
+			this.opt.magnific.each(function(){ 
+				$(this).magnificPopup(main.opt.magnificConf);
+			});
+
+			// this.rangeInput();
+			this.logIn();
+			this.filter();
+
+		}
+	};
+	main.init();
+
+	// $(document).ready(function(){
+
+	// });
+
+	
+	// $("form").submit(function() {
+	// 	var th = $(this);
+	// 	$.ajax({
+	// 		type: "POST",
+	// 		url: "mail.php", 
+	// 		data: th.serialize()
+	// 	}).done(function() {
+	// 		alert("Thank you!");
+	// 		setTimeout(function() {				
+	// 			th.trigger("reset");
+	// 		}, 1000);
+	// 	});
+	// 	return false;
+	// });
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -7,30 +146,6 @@ $(function() {
 		});
 	};
 	
-	$("form").submit(function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", 
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {				
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-	$('.popup-slider').each(function() { 
-		$(this).magnificPopup({
-			delegate: 'a',
-			type: 'image',
-			gallery: {
-				enabled:true
-			}
-		});
-	});
 	
 	try {
 		$.browserSelector();
@@ -39,33 +154,54 @@ $(function() {
 		}
 	} catch(err) {
 
-	};
+	};	
+});
 
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+$(document).ready(function() {
+  var doubleHandleSlider = document.querySelector('.double-handle-slider');
+	var minValInput = document.querySelector('.min-value');
+	var maxValInput = document.querySelector('.max-value');
 
-	//Анимация логин регистрация
-	$('.autoriz a').click(function(event) {
-		event.preventDefault();
-		var t        = $(this),
-			dataForm = t.data('form'),
-			parrent  = t.parents('.autoriz');
+	
+	noUiSlider.create(doubleHandleSlider, {
+		start: [ 0, 100 ],
+		connect: true,
+		tooltips: false,
+		step: 1,
+		range: {
+			'min': [ 0 ],
+			'max': [ 100 ]
+		},
+		format: {
+			to: function(value) {
+				return value;
+			},
+			from: function(value) {
+				return value;
+			}
+		}
+	});	
+	doubleHandleSlider.noUiSlider.on('change', function( values, handle ) { 			
+		var rangeValues = values;
 
-		$('.autoriz a').not(t).removeClass('active');
-		toggleC(t);
-		parrent.find('form').not('#'+dataForm).removeClass('active');			
-		toggleC('#'+dataForm);
-	});
-
-	//Анимация фильтр
-	$('#search').on('click',function(){
-		toggleC(this);
-		toggleC('.filters');
-		// $(this).toggleClass('active');
-	});
-
-
-	function toggleC(el){
-		$(el).toggleClass('active');
-	};
+			minValInput.value = rangeValues[0];
+			maxValInput.value = rangeValues[1];
+			
+			// This version updates a single input on change
+			var val = values[handle]; // 0 or 1
+			
+			if(handle) {
+				maxValInput.value = Math.round(val);
+			} else {
+				minValInput.value = Math.round(val);
+			}
+	});	
+		minValInput.addEventListener('change', function(){
+			doubleHandleSlider.noUiSlider.set([this.value, null]);
+		});
+			
+		maxValInput.addEventListener('change', function(){
+			doubleHandleSlider.noUiSlider.set([null, this.value]);
+		});
 });
